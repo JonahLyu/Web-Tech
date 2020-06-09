6366db32d2f0aed570e769cb6167d7a6a136994b
Aargno <robey.aaron98@gmail.com>
2020-06-09 16:17:52 +0100

    Error checking on user routes

--- routes/users.js
+++ routes/users.js
@@ -40,13 +40,12 @@ router.get('/listUsers', secured, function(req, res, next) {
 router.get('/allProfiles', secured, function(req,res,next) { 
   console.log(req.session.user);
   try {
-    // throw err;
     userDAO.getAllUser((users) => {
       //Add delete functionality
       res.send(users);
     });
   } catch (err) {
-    res.redirect('/forum/home');
+    res.send();
   }
 })
 
@@ -58,45 +57,62 @@ router.get('/setting', secured, function(req, res, next) {
     if (req.query.id) { //If we recieve a query at all
       let access = ((req.session.user.level === 3) || (req.session.user.id === req.query.id)) && (req.query.id != -1) //Restrict access to admins, or people who own the page
       userID = (access) ? req.query.id : req.session.user.id; //If no access granted send user to their settings page
+    } else {
+      res.redirect('/forum/home');
+      return next();
     }
-    userDAO.getUser(userID, (result) => {
-      if (!result) { //User sets up own page
-        res.render("setting", {title: "Setting",
-          userID: userID,
-          userProfile: userProfile,
-          level: 1,
-          accessLevel: req.session.user.level});
-      } else if (result) {
-        res.render("setting", {title: "Setting",
-          userID: userID,
-          userProfile: userProfile,
-          username: result.Username,
-          gender: result.Gender,
-          birthday: result.Birthday,
-          phone: result.Phone,
-          level: result.Level,
-          accessLevel: req.session.user.level});
-      } else {
-        console.log("Can't edit non-existant profile")
-      }
-    });
+    try {
+      userDAO.getUser(userID, (result) => {
+        if (!result) { //User sets up own page
+          res.render("setting", {title: "Setting",
+            userID: userID,
+            userProfile: userProfile,
+            level: 1,
+            accessLevel: req.session.user.level});
+        } else if (result) {
+          res.render("setting", {title: "Setting",
+            userID: userID,
+            userProfile: userProfile,
+            username: result.Username,
+            gender: result.Gender,
+            birthday: result.Birthday,
+            phone: result.Phone,
+            level: result.Level,
+            accessLevel: req.session.user.level});
+        } else {
+          console.log("Can't edit non-existant profile")
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/forum/home');
+    }
     // res.render("setting", {title: "Setting", userProfile: userProfile});
 });
 
 
 router.post("/info", secured, (req, res) => {
     // req.session.user == userProfile.id now
-    const { _raw, _json, ...userProfile } = req.user;
-    var userID = req.session.user.id;
-    userDAO.getUser(userID, (result) => {
-      res.send(result)
-    });
+    // const { _raw, _json, ...userProfile } = req.user;
+    try {
+      var userID = req.session.user.id;
+      userDAO.getUser(userID, (result) => {
+        res.send(result)
+      });
+    } catch (err) {
+      console.log(err);
+      res.send();
+    }
 })
 
 
 router.post('/save_setting', secured, function(req, res, next) {
   //More validation here
   const { _raw, _json, ...userProfile } = req.user;
+  if (!req.body.user_id || !req.body.username || !req.body.gender || !req.body.birthday || !req.body.level) {
+    res.status(403).send("/forum/home");
+    return next();
+  }
   let access = ((req.session.user.level === 3) || (req.session.user.id === req.body.user_id)) //Restrict access to admins, or people who own the page
   if (!access) {
     res.status(403).send("/forum/home");
@@ -112,36 +128,53 @@ router.post('/save_setting', secured, function(req, res, next) {
   level = (level) ? level : 1; //Ensures that if level is undefined it is set to default level: 1.
   let sql = `select * from users where UserID = ?`;
   req.session.user.level = (req.session.user.level) ? req.session.user.level : level;
-  db.all(sql, id, (err, results) => {
-    if (err) {
-      throw err;
-    }
-    console.log(id);
-    console.log(results);
-    if (results.length == 0 && req.session.user.id === req.body.user_id) {
-        //send the original auth id to the createUser function
-        userDAO.createUser(id, username, birthday ,gender, phone, level, req.session.user.user_id)
-        res.status(201).send(201);
-    }
-    else if (results.length != 0){
-        userDAO.updateUser(id, username, birthday ,gender, phone, level)
-        res.status(201).send(200);
-    }
-    else {
-      console.log("User needs to create profile first")
-    }
-  });
+  try {
+    db.all(sql, id, (err, results) => {
+      if (err) {
+        throw err;
+      }
+      console.log(id);
+      console.log(results);
+      if (results.length == 0 && req.session.user.id === req.body.user_id) {
+          //send the original auth id to the createUser function
+          userDAO.createUser(id, username, birthday ,gender, phone, level, req.session.user.user_id)
+          res.status(201).send(201);
+      }
+      else if (results.length != 0){
+          userDAO.updateUser(id, username, birthday ,gender, phone, level)
+          res.status(201).send(200);
+      }
+      else {
+        console.log("User needs to create profile first")
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(403).send("/forum/home");
+  }
 });
 
 
 router.post("/userInfo", secured, (req, res) => {
+    if (!req.body.userID) {
+      res.send();
+      return next();
+    }
     var userID = req.body.userID;
-    userDAO.getOtherUser(userID, (result) => {
-      res.send(result)
-    });
+    try {
+      userDAO.getOtherUser(userID, (result) => {
+        res.send(result)
+      });
+    } catch (err) {
+      res.send();
+    }
 })
 
 router.post("/deleteUser", secured, (req, res) => {
+  if (!req.body.userID) {
+    res.send("/");
+    return next();
+  }
   var management = new ManagementClient({
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENT_ID,
@@ -149,25 +182,29 @@ router.post("/deleteUser", secured, (req, res) => {
     scope: 'delete:users create:users'
   });
   if (req.session.user.level === 3 || req.body.userID === req.session.user.id) {
-    userDAO.getUser(req.body.userID, (user) => {
-      console.log(user.Username);
-      management.users.delete({ id: user.AuthID }, function (err) {
-        if (err) {
-          console.log(err)
-        }
-        console.log("User deleted");
-        userDAO.deleteUser(req.body.userID);
-        postDAO.deletePostByUser(req.body.userID);
-        comDAO.deleteComByUserID(req.body.userID);
-        if (req.session.user.level === 3 && req.body.userID !== req.session.user.id) { //If admin, send to home page
-          res.send("/");
-        } else { //If user force re-log as only time we rach here is when user has deleted own account
-          req.session.user = null;
-          req.session.returnTo = "/";
-          res.send("/login");
-        }
-      });
-    })
+    try {
+      userDAO.getUser(req.body.userID, (user) => {
+        console.log(user.Username);
+        management.users.delete({ id: user.AuthID }, function (err) {
+          if (err) {
+            console.log(err)
+          }
+          console.log("User deleted");
+          userDAO.deleteUser(req.body.userID);
+          postDAO.deletePostByUser(req.body.userID);
+          comDAO.deleteComByUserID(req.body.userID);
+          if (req.session.user.level === 3 && req.body.userID !== req.session.user.id) { //If admin, send to home page
+            res.send("/");
+          } else { //If user force re-log as only time we rach here is when user has deleted own account
+            req.session.user = null;
+            req.session.returnTo = "/";
+            res.send("/login");
+          }
+        });
+      })
+    } catch (err) {
+      res.send("/logout");
+    }
   } else {
     res.send("/");
   }
